0f8192553e843f27d5a4698138abc8ff308fb6b3
Raul Kaio Silva <rkaio18@gmail.com>
2017-07-26 21:29:39 -0300

    Alterações: - Retirei o menu lateral; - Incluí validação do javascript da página da prefeitura, isso fez com que fosse possível tanto dar post no formulário quanto receber os dialogs de alerta quando erra o captcha por exemplo; - Alterei a cor verde secundária para um verde mais claro; - Inclui o firebase e o play services; - Inclui um loading de página ao iniciar a aplicação; - Inclui o Admob mas ainda não aparece na aplicação.

--- app/src/main/java/br/com/raulkaio/iptugoianiaboletosduam/MainActivity.java
+++ app/src/main/java/br/com/raulkaio/iptugoianiaboletosduam/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.raulkaio.iptugoianiaboletosduam;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,12 +16,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity{
 
     private class MyWebViewClient extends WebViewClient {
         @Override
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity
 
     Button btnBack;
     WebView webview;
+    private static final String TAG = "Main";
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,42 +46,61 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // Chamada do bloco de anúncois
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-5266434890042499/3904545441");
 
         webview=(WebView)findViewById(R.id.webview1);
         webview.setWebViewClient(new MyWebViewClient());
         webview.getSettings().setLoadWithOverviewMode(true);
+        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+        //Habilita o uso de javascript, fez com que a aplicação conseguisse enviar os formulários para o site da prefeitura
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setWebChromeClient(new WebChromeClient());
+
         //Diminui o conteúdo trazido, como se estivesse no desktop, faz os itens na tela caberem todos de uma vez
         //webview.getSettings().setUseWideViewPort(true);
-        openURL();
-    }
 
-    private void openURL() {
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Carregando o conteúdo...");
+        progressDialog.show();
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(MainActivity.this, "Error:" + description, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         webview.loadUrl("https://www.goiania.go.gov.br/sistemas/scarr/asp/scarr33000f0.asp");
-        webview.requestFocus();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }*/
+
+        if (webview.canGoBack()) {
+            webview.goBack();
         } else {
             super.onBackPressed();
         }
@@ -93,35 +121,14 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_feedback) {
             return true;
+        }*/
+        if (id == R.id.action_sair) {
+            finish();
+            System.exit(0);
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        /*int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);*/
-        return true;
     }
 }
